70eb94097503d6b00f2133b86667e4e6f6bcf818
Mr Footwork <pandau.ting@gmail.com>
2024-10-25 00:22:49 +0200

    iteration 3: fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import './App.css';
 
 function App() {
-	return <div className='App'></div>;
+	return (
+		<div className='App'>
+			<h1>TEST</h1>
+			<p>TEST</p>
+		</div>
+	);
 }
 
 export default App;
