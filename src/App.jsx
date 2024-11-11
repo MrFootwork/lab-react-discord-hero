9be44027534279d8582dc0cb630f8916bcdc3724
Mr Footwork <pandau.ting@gmail.com>
2024-11-11 14:53:27 +0100

    🎨 style landing page

--- src/App.jsx
+++ src/App.jsx
@@ -15,15 +15,17 @@ function App() {
 			</nav>
 
 			<article>
-				<h1>Imagine a place...</h1>
+				<header>
+					<h1>Imagine a place...</h1>
+				</header>
 				<p>
 					...where you can belong to a school club, a gaming group, or a
 					worldwide art community. Where just you and a handful of friends can
 					spend time together. A place that makes it easy to talk every day and
 					hang out more often.
 				</p>
-				<button>Download for Mac</button>
-				<button>Open Discord in your browser</button>
+				<button className='light'>Download for Mac</button>
+				<button className='dark'>Open Discord in your browser</button>
 			</article>
 		</div>
 	);
